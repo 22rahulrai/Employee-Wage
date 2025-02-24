fff3c5ecf4441e85e8ffc9673208d393d87f6127
Rahul Rai <rairayrahul22@gmail.com>
2025-02-24 10:53:19 +0530

    uc-4

--- empoyee-wage/Program.cs
+++ empoyee-wage/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dev branch for usecases");
+            Console.Write("Enter no. of hours: ");
+            int hours = int.Parse(Console.ReadLine());
+            int empWage = 0;
+            int emp_rate_per_hour = 200;
+            switch (hours)
+            {
+                case 1:
+                    empWage = hours * emp_rate_per_hour;
+                    Console.WriteLine("Your part-time wage is: " + empWage);
+                    break;
+                case 2:
+                    empWage = hours * emp_rate_per_hour;
+                    Console.WriteLine("Your part-time wage is: " + empWage);
+                    break;
+                case 3:
+                    empWage = hours * emp_rate_per_hour;
+                    Console.WriteLine("Your part-time wage is: " + empWage);
+                    break;
+
+                case 4:
+                    empWage = hours * emp_rate_per_hour;
+                    Console.WriteLine("Your part-time wage is: "+empWage);
+                    break;
+
+                case 8:
+                    empWage = hours * emp_rate_per_hour;
+                    Console.WriteLine("Your full-time wage is:" + empWage);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input.");
+                    break;
+            }
+            
         }
     }
 }
