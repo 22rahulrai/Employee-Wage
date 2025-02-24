94d82f47d9ddc300d6c8d37d5e48a3655b556d5b
Rahul Rai <rairayrahul22@gmail.com>
2025-02-24 10:26:26 +0530

    uc-2

--- empoyee-wage/Program.cs
+++ empoyee-wage/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dev branch for usecases");
+            Console.Write("Enter 1 if the employee is present, otherwise enter 0: ");
+            int emp_present = int.Parse(Console.ReadLine());
+            Console.WriteLine(emp_present);
+
+            int emp_rate_per_hour = 100;
+
+            int emp_hour = 0;
+            int emp_salary = 0;
+
+            if (emp_present == 1)
+            {
+                emp_hour = 7;
+            }
+            else
+            {
+                emp_hour = 0;
+            }
+
+            emp_salary = emp_rate_per_hour * emp_hour;
+            Console.WriteLine("Employee salary : " + emp_salary);
         }
     }
 }
