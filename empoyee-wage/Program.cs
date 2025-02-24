526c9c41a833f82769af429fc90910361aec5300
Rahul Rai <rairayrahul22@gmail.com>
2025-02-24 10:16:41 +0530

    usecase1

--- empoyee-wage/Program.cs
+++ empoyee-wage/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dev branch for usecases");
+            Console.WriteLine("1 is for present and 0 for absent ");
+            int attendance = int.Parse(Console.ReadLine());
+
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
