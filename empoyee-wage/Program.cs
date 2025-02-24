a587efb5f84cb73435c7ef58a4df59cfcc5d6a86
Rahul Rai <rairayrahul22@gmail.com>
2025-02-24 10:10:34 +0530

    add dev

---
+++ empoyee-wage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace empoyee_wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dev branch for usecases");
+        }
+    }
+}
