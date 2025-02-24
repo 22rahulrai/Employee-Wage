c207bb6bb314195d82caf8514bdaa5355c4c9613
Rahul Rai <rairayrahul22@gmail.com>
2025-02-24 10:42:40 +0530

    uc-3

--- empoyee-wage/Program.cs
+++ empoyee-wage/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Dev branch for usecases");
+            Console.WriteLine("Enter no of hours you work");
+
+            int emp_hours = int.Parse(Console.ReadLine());
+            int emp_wage = 0;
+            int emp_rate_hour = 200;
+
+            if (emp_hours <= 4){
+                emp_wage=emp_hours*emp_rate_hour;
+                Console.WriteLine("Your Part time wage is :" +emp_wage);
+            }
+            else if(emp_hours == 8)
+            {
+                emp_wage = emp_hours * emp_rate_hour;
+                Console.WriteLine(" Your full time wage is: "+emp_wage);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
         }
     }
 }
